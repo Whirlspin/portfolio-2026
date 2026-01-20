fd2850862f585ac26751e38fc1df8875da5a165a
Jethro Edwards <89199617+Whirlspin@users.noreply.github.com>
2026-01-20 13:41:28 +0000

    Jan x6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
-import Script from "next/script";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,6 +23,18 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} antialiased`}>{children}</body>
       <GoogleAnalytics gaId="G-MRFBSZXSWD" />
+
+      {/* Hotjar  */}
+      <Script strategy="afterInteractive">
+        {`(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:6625273,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </Script>
     </html>
   );
 }
