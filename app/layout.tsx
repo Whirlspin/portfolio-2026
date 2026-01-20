d62d161cc64c5b8de4be5b5c95424f08b58ed6ad
Jethro Edwards <89199617+Whirlspin@users.noreply.github.com>
2026-01-20 13:19:22 +0000

    January x4

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,13 +25,13 @@ export default function RootLayout({
       <GoogleAnalytics gaId="G-MRFBSZXSWD" />
       <Script strategy="afterInteractive">
         {`(function(h,o,t,j,a,r){
-    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-    h._hjSettings={hjid:6625273,hjsv:6};
-    a=o.getElementsByTagName('head')[0];
-    r=o.createElement('script');r.async=1;
-    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-    a.appendChild(r);
-  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:6625273,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+       })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
       </Script>
     </html>
   );
