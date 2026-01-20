058193a24aa56f58627f149eb35a64496f8d1e30
Jethro Edwards <89199617+Whirlspin@users.noreply.github.com>
2026-01-20 13:11:32 +0000

    January update x3

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,16 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} antialiased`}>{children}</body>
       <GoogleAnalytics gaId="G-MRFBSZXSWD" />
-      <Script src="/projects/hotjar.js" strategy="afterInteractive" />
+      <Script strategy="afterInteractive">
+        {`(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:6625273,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </Script>
     </html>
   );
 }
